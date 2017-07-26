4b4ac47748b3850ca895fb49e8449ac7a657f501
Tony Gunn <khiidt95@gmail.com>
2017-07-26 14:03:55 -0500

    update

---
+++ src/pages/projects/projects.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ProjectsPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-projects',
+  templateUrl: 'projects.html',
+})
+export class ProjectsPage {
+  projects:any= [
+    {
+    name:'Tony',
+    description: 'we work here',
+    image:'img/tony.jpg'
+    },
+      {
+        name:'Gunn',
+        description:'XYZZ',
+        image:''
+      },
+      {
+        name:'Claim Academy',
+        description: 'XYZZY',
+        image:''
+      }
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ProjectsPage');
+  }
+
+    test(project){
+      console.log(project);
+    }
+}
